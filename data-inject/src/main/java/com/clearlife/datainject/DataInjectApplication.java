a48c07720e272343c4a35de052dfa2c2fb428edd
mingsheng zhang <mszhanghm@gmail.com>
2023-03-09 15:41:21 -0500

    add data-inject module

---
+++ data-inject/src/main/java/com/clearlife/datainject/DataInjectApplication.java
@@ -0,0 +1,13 @@
+package com.clearlife.datainject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataInjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataInjectApplication.class, args);
+    }
+
+}
---
+++ data-inject/src/test/java/com/clearlife/datainject/DataInjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clearlife.datainject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataInjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
