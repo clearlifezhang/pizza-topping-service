121eff800c511dbaaec24567d8d6b87834349274
mingsheng zhang <mszhanghm@gmail.com>
2023-03-09 14:17:58 -0500

    add topping-service module to the project

---
+++ topping-service/src/main/kotlin/com/clearlife/toppingservice/ToppingServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.clearlife.toppingservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToppingServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ToppingServiceApplication>(*args)
+}
