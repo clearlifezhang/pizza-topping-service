4fda9240f20ac04ae468a331e7dea4ea349e061d
mingsheng zhang <mszhanghm@gmail.com>
2023-03-09 16:20:15 -0500

    Add RedisTemplate

--- data-inject/src/main/java/com/clearlife/datainject/DataInjectApplication.java
+++ data-inject/src/main/java/com/clearlife/datainject/DataInjectApplication.java
@@ -2,6 +2,10 @@ package com.clearlife.datainject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
 
 @SpringBootApplication
 public class DataInjectApplication {
